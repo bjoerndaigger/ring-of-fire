53604da5e491bc3bf54a231ce2cc3506aa9ff3a1
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-09-19 16:58:47 +0200

    game cards added

--- src/models/game.ts
+++ src/models/game.ts
@@ -3,5 +3,14 @@ export class Game {
     public stack: string[] = []; // unplayed cards
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('spade_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        };
+    }
 }
 
