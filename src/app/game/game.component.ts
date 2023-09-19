f390a50cda256204257fca5dfaafe6e835078f7f
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-09-19 21:55:22 +0200

    show various cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,6 +9,7 @@ import { Game } from 'src/models/game';
 
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;
 
   ngOnInit() {
@@ -21,7 +22,17 @@ export class GameComponent {
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500)
+    }
   }
 }
+
+
 
--- src/models/game.ts
+++ src/models/game.ts
@@ -10,7 +10,25 @@ export class Game {
             this.stack.push('hearts_' + i);
             this.stack.push('clubs_' + i);
             this.stack.push('diamonds_' + i);
-        };
+        }
+
+        shuffle(this.stack);
     }
 }
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+  
